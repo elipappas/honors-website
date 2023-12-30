6c59cdc01f081134097250d2187346beded00392
elipappas <eli@bpi.net>
2023-12-29 19:53:45 -0500

    removed apostrophes

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,14 +1,12 @@
-import Link from 'next/link'
-
 export default function About() {
     return (
         <div className="flex justify-center">
             <div className="pr-8">
-                <img src='/IMG_3056.JPG' width="3000"/>
+                <img src="/IMG_3056.JPG" width="3000"/>
             </div> 
             <div>
                 <p className="w-full">
-                    Hello, I'm Eli Pappas, I'm a second year Computer Science Major at the University of Cincinnati. I'm from Cincinnati Ohio. My interests include computers, video games, reading, basketball, golf, lifting, and watching pretty much any sport. In highschool I played on the golf team as well as threw shotput and discus. I was also was a part of my school TEAMS competition for physics and math. I currently plan to get a minor in math as well as an MBA and I hope to end up working for a tech company where I can make an impact on people's daily lifes and try to improve existing and create new technologies. My strengths include leadership, adaptability, and I am always looking to improve myself I hope to use these strengths in order to innovate and contribute to new technologies. Through the honors program I am looking to broaden my scope of the world and get more unique experiences than what I have gotten so far from before coming to the UC honors program.
+                    Hello, I&apos;m Eli Pappas, I&apos;m a second year Computer Science Major at the University of Cincinnati. I&apos;m from Cincinnati Ohio. My interests include computers, video games, reading, basketball, golf, lifting, and watching pretty much any sport. In highschool I played on the golf team as well as threw shotput and discus. I was also was a part of my school TEAMS competition for physics and math. I currently plan to get a minor in math as well as an MBA and I hope to end up working for a tech company where I can make an impact on people&apos;s daily lifes and try to improve existing and create new technologies. My strengths include leadership, adaptability, and I am always looking to improve myself I hope to use these strengths in order to innovate and contribute to new technologies. Through the honors program I am looking to broaden my scope of the world and get more unique experiences than what I have gotten so far from before coming to the UC honors program.
                 </p>
             </div>
             
--- src/app/Experiences/page.tsx
+++ src/app/Experiences/page.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link'
-
 export default function HonorsExperience() {
     return (
         <div>
